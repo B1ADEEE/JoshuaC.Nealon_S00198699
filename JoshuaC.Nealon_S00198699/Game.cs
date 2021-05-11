4ec76a1dd6fc12915af21397f1c14240a1e2a826
Joshua Crowley Nealon <S00198699@mail.itsligo.ie>
2021-05-11 09:49:26 +0100

    Step 2 Done

---
+++ JoshuaC.Nealon_S00198699/Game.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoshuaC.Nealon_S00198699
+{
+    class Game
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public int CriticScore { get; set; }
+        public string Description{ get; set; }
+
+        public string Platform { get; set; }
+        public string GameImage { get; set; }
+        public decimal Price { get; set; }
+
+        public Game()
+        {
+
+        }
+
+        public Game(string name, decimal price, int criticscore, string description,string platform, string gameImage = "")
+        {
+            Name = name;
+            Price = price;
+            CriticScore = criticscore;
+            Description = description;
+            Platform = platform;
+            GameImage = gameImage;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public void DecreasePrice(double increase)
+        {
+            Price *= (decimal)(1 - increase);
+        }
+    }
+}
