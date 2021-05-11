3411177a3aaa4fcf26465043e01102cf1d0f0f05
Joshua Crowley Nealon <S00198699@mail.itsligo.ie>
2021-05-11 12:24:56 +0100

    done v2

--- JoshuaC.Nealon_S00198699/MainWindow.xaml.cs
+++ JoshuaC.Nealon_S00198699/MainWindow.xaml.cs
@@ -18,6 +18,13 @@ namespace JoshuaC.Nealon_S00198699
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+
+    /*
+     * 
+     * ---- GITHUB https://github.com/B1ADEEE/JoshuaC.Nealon_S00198699
+     */
     public partial class MainWindow : Window
     {
         List<Game> AllGames;
@@ -27,15 +34,15 @@ namespace JoshuaC.Nealon_S00198699
         }
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            GameData db = new GameData();
-            var query = from g in db.Games
+            GameData db = new GameData();                                                                           
+            var query = from g in db.Games                                                              //query to get the games into the tbx
                         select g;
             AllGames = query.ToList();
             GamesLBX.ItemsSource = AllGames;
         }
         private void GamesLBX_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            Game selectedGame = GamesLBX.SelectedItem as Game;
+            Game selectedGame = GamesLBX.SelectedItem as Game;                                                          //selecting the games to get the image and the desc
             if (selectedGame != null)
             {
                 GameIMG.Source = new BitmapImage(new Uri(selectedGame.GameImage, UriKind.Relative));
@@ -43,19 +50,18 @@ namespace JoshuaC.Nealon_S00198699
             }
         }
 
-        private void FilterDrop_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void FilterDrop_SelectionChanged(object sender, SelectionChangedEventArgs e)        //filters the games to platform
         {
-            Game selectedPlatform = FilterDrop.SelectedItem as Game;
+            Game selectedPlatform = FilterDrop.SelectedItem as Game;                                //this crashes once but if your press continue it will work again.. seems to happen everytime you reselt a new filter for the fist time it wont work
             GameData db = new GameData();
-            var query = from g in db.Games
+            var query = from g in db.Games                                                          
                         where g.Platform != null && g.Platform == selectedPlatform.Platform
                         select g;
-                GamesLBX.ItemsSource = query.ToList();
+            GamesLBX.ItemsSource = query.ToList();
         }
 
-        private void FilterDrop_DropDownOpened(object sender, EventArgs e)
+        private void FilterDrop_DropDownOpened(object sender, EventArgs e)                          //dropdown menu for filtering
         {
-            
             GameData db = new GameData();
             var query = from g in db.Games
                         select g;
